c43b54a0e3dec33b68c135f1b54c21881e305ce3
Jerad Acosta <jerad@jerads-imac.marin.digitalfoundry.com>
2015-07-24 10:40:34 -0700

    issue with 'invalid multibyte string 1' appears to be an encoding problem on row 15 of Summary Column

--- storyPoints.R
+++ storyPoints.R
@@ -8,7 +8,13 @@
 # * = grab as many as you can before a space
 # $ = ends line
 
+### Temp Path: /Users/jerad/Documents/Rcode/JIRASubtastVelocity/sprint1.csv
+
 storyPoints <- function(path){
+  ## Libraries
+  # stringr for extracting numbers from summary
+  # library('stringr')
+  
   # load CSV file into memory
   data <- read.csv(path)
   
@@ -20,4 +26,30 @@ storyPoints <- function(path){
   colnames(data) <- unlist(data[1,])
   # remove first - repeat of header / col names
   data <- data[-1,]
+  
+  # iterate through summary column to extract story points into new variable / column
+  for (i in data$Summary) {
+    data$storyPoints <- extract(i)
+  }
+  
+}
+
+
+extract <- function(summary){
+  ## Extract Story Points from Summary
+  # [0-9]+$ = regex for last number with length > 0
+  # gsub('[0-9]+$','\\1',i) = extracts the entire string not matching [0-9]+$ pattern
+  # substr() takes out everything from the original string except what we want
+  
+  # coerce factor variable to character vector
+  summary <- as.character(summary)
+  pattern <- '[0-9]+$'
+  # create the inverse of the desired string characters
+  inverseString <- gsub(pattern, '\\1', summary)
+  # subset original string by removing the inverseString from it
+  string <- substr(summary, nchar(inverseString), nchar(summary))
+  
+  # coerse desired characters from string into numeric for column input
+  num <- as.numeric(string)
+  return(num)
 }
